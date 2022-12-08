14d41a530d508dc4f3abbbcd32588fdacb7acb73
VMDang <dang.vm205063@sis.hust.edu.vn>
2022-12-08 11:57:31 +0700

    add class Store and StoreTest

--- AimsProject/src/Aims.java
+++ AimsProject/src/Aims.java
@@ -1,9 +1,9 @@
 public class Aims {
     public static void main(String[] args) {
         Cart list = new Cart();
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("John Wick", "action", "Adam Smith", 4, 15.99f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Shin", "animation", "Tanaka", 99, 6.6f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Avenger", "Science Fiction", "Marvel", 5, 30f);
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
 
         list.addDigitalVideoDisc(dvd1);
         list.addDigitalVideoDisc(dvd2);
---
+++ AimsProject/src/StoreTest.java
@@ -0,0 +1,19 @@
+public class StoreTest {
+    public static void main(String[] args) {
+        Store st = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+
+        st.addDVD(dvd1);
+        st.addDVD(dvd2);
+        st.addDVD(dvd3);
+
+        System.out.println("The store before remove:");
+        st.displayStore();
+
+        st.removeDVD(dvd2);
+        System.out.println("The store after remove:");
+        st.displayStore();
+    }
+}
